c92be21e4d9b17511580bf5d7a718aeb223678f9
WillSilvawts <willsilvawts@gmail.com>
2022-08-24 15:19:56 -0400

    novas atualizações

--- dia-2-github/pratica-1.js
+++ dia-2-github/pratica-1.js
@@ -4,8 +4,8 @@ let profissao = "Desenvolvedor";
 let idade = 33;
 let lugar = "Canada";
 
-console.log (`Olá,`  + nome + `! Somos aqui da agência de turismo Viagem dos Sonhos. 
-Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para 
+console.log (`Olá,`  + nome + `! Somos aqui da agência de turismo Viagem dos Sonhos.
+Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para
 o ` +  lugar + `. Confirma pra gente alguns dados? A sua idade é ` + idade + `, você é`  + email + `?
  Ficamos no aguardo. Muito obrigado e boa viagem! :D`);
 
