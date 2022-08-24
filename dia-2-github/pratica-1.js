194a6679c2679cf1969ae860314789f55c133932
WillSilvawts <willsilvawts@gmail.com>
2022-08-24 13:14:46 -0400

    criando variaveis

--- dia-2-github/pratica-1.js
+++ dia-2-github/pratica-1.js
@@ -3,3 +3,7 @@ let email = "willsilva@meuemail.com";
 let profissao = "Desenvolvedor";
 let idade = 33;
 let lugar = "Canada";
+
+console.log (nome , "  " , email + "  " ,  profissao + "  " ,  idade + "  " ,  lugar , "  " );
+
+
